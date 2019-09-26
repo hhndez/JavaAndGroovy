090138990615610f58abc8741b94ed58d5f42cf5
Hugo Hernandez <hhernandez@SIDERO.Athlone.Local>
2019-09-26 11:17:15 +0100

    Cleaning the house

---
+++ src/main/java/com/lessons/Conversion.java
@@ -0,0 +1,26 @@
+package com.lessons;
+
+public class Conversion {
+
+    public static void main (String ...args) {
+        byte b;
+        int i = 258;
+        double d = 323.142;
+
+        System.out.println("\nConversion of int to byte.");
+        b = (byte) i;
+        System.out.println("i and b " + i + " " + b);
+
+        int num1[], num2, num3[];
+
+        var avg = 10.0;
+        var myString = "";
+
+        int monthDays[] = {31, 28, 31, 30};
+
+        //var  myVar = {10, 20, 30};    wrong
+        var test = myString;
+
+
+    }
+}
---
+++ src/main/java/com/lessons/Types.java
@@ -0,0 +1,9 @@
+package com.lessons;
+
+public class Types {
+    public static void main(String ...args) {
+        int abc = 2749;
+        int binary = 0b1111_1111_00001;
+        float floatnumber = (float) 0.11111E+1;
+    }
+}
